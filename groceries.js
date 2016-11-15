4fc375f158ff027992190bb4187325f4268e53fc
KOS-MACHINE\kaustubh chaudhuri <kchaudhuri@mail.csuchico.edu>
2016-11-14 16:46:08 -0800

    added remove button to the list.

--- groceries.js
+++ groceries.js
@@ -5,8 +5,20 @@ function addItem()
   var input = document.getElementById('newItem').value;
   var list = document.getElementById('listDisplay');
   var item = document.createElement('li');
+
+  var btnClose = document.createElement('button');
+  btnClose.classList.add("btn");
+  btnClose.classList.add("btn-danger");
+  btnClose.classList.add("btn-xs");
+  var iconClose = document.createElement('span');
+  iconClose.classList.add("glyphicon");
+  iconClose.classList.add("glyphicon-remove");
+  btnClose.appendChild(iconClose);
+
+
   var itemName = document.createTextNode(input);
   item.appendChild(itemName);
+  item.appendChild(btnClose);
   list.appendChild(item);
   document.getElementById('newItem').value = "";
 }
